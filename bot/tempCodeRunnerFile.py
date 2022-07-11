6a6e874ddfe53d36653c9aa74a39a9aa8ddc4202
luke-f <lukedfairbanks@gmail.com>
2022-07-11 11:51:12 -0600

    Deleted tempCodeRunnerFile

--- bot/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-desc1 = f"""
-# 🔥The rundown for {datetime.datetime.now().strftime("%d %b, %Y")}:
-# #1️⃣ goes to.. {data[0]['username']} - {data[0]['sound_used']} {data[0]['is_verified']}
-# {data[0]['video_url']}
-# """
